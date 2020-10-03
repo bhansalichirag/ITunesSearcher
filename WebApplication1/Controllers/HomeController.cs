8cf599c761a849f224ae565b63f947ea46e9a035
Chirag <bhansali.chirag@hotmail.com>
2020-10-03 16:21:54 -0700

    ClickInfo, database insert

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -12,10 +13,12 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -25,6 +28,13 @@ namespace WebApplication1.Controllers
             ViewData["heading"] = "Welcome to ASP.NET Core MVC !!";
             return View(emp);
         }
+        
+        public void RecordClick(string str1,string str2)
+        {
+            ClickInfo obj = new ClickInfo { count = 1, TrackName = str1, TrackURL = str2 };
+            _db.ClickInfo.Add(obj);
+            _db.SaveChanges();
+        }
 
         public IActionResult Privacy()
         {
@@ -33,7 +43,14 @@ namespace WebApplication1.Controllers
 
         public IActionResult ClickInfo()
         {
-            return View();
+            List<ClickInfo> objList = _db.ClickInfo.GroupBy(o => new { o.TrackName, o.TrackURL })
+                .Select(g => new ClickInfo
+                {
+                    TrackName = g.Key.TrackName,
+                    count = g.Count(),
+                    TrackURL = g.Key.TrackURL
+                }).ToList();
+            return View(objList);
         }
 
         public IActionResult Info()
